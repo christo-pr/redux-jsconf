541a8839f259471489133c48197eed015388a458
Christofer Flores <cristofer.flort@gmail.com>
2020-10-12 20:28:49 -0500

    Create a custom hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 
 import { StyledCounter } from "styles/"
-
-// Counter timer
-var timer
+import { useCounter } from "hooks/"
 
 export function Counter(props) {
   const { duration } = props
-  const [counter, setCounter] = useState(0)
-  const [active, setActive] = useState(true)
-
-  // Start timer
-  useEffect(() => {
-    timer = setInterval(() => {
-      setCounter((counter) => counter + 1)
-    }, 1000)
-
-    return () => {
-      clearInterval(timer)
-    }
-  }, [])
-
-  // Stop timer
-  useEffect(() => {
-    if (counter > duration) {
-      setCounter(0)
-      setActive(false)
-      clearInterval(timer)
-    }
-  }, [counter, timer])
+  const { counter, active } = useCounter(duration)
 
   return <StyledCounter active={active}>{counter}</StyledCounter>
 }
