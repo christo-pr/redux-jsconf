5e18032ab6ce65c67edd944ed148d8dcf8e7fabc
Christofer Flores <cristofer.flort@gmail.com>
2020-10-24 09:39:21 -0500

    Updates

---
+++ precache-manifest.d2de741f0ba56268215f5f46ea90c329.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43017f62ba23c3e37a9cb67100dc74dc",
+    "url": "/redux-jsconf/index.html"
+  },
+  {
+    "revision": "4c8dd208ae82e74bc6d4",
+    "url": "/redux-jsconf/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9221fa413f4a61ca1212",
+    "url": "/redux-jsconf/static/css/main.ca0b0216.chunk.css"
+  },
+  {
+    "revision": "4c8dd208ae82e74bc6d4",
+    "url": "/redux-jsconf/static/js/2.eed7a9ee.chunk.js"
+  },
+  {
+    "revision": "15ae086c36af272d8eff976ce9d43026",
+    "url": "/redux-jsconf/static/js/2.eed7a9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9221fa413f4a61ca1212",
+    "url": "/redux-jsconf/static/js/main.29e94ac8.chunk.js"
+  },
+  {
+    "revision": "3c8c1c4fc544d8be8bbf",
+    "url": "/redux-jsconf/static/js/runtime-main.6f2c14b6.js"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/redux-jsconf/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  },
+  {
+    "revision": "35ebd92bb2411d90be32bd4348e19ea6",
+    "url": "/redux-jsconf/static/media/background-game.35ebd92b.png"
+  },
+  {
+    "revision": "dd662deb928a501fa22919841e32aedb",
+    "url": "/redux-jsconf/static/media/heart-empty.dd662deb.png"
+  },
+  {
+    "revision": "fbbe3f464ff5651f468fda4e358a236e",
+    "url": "/redux-jsconf/static/media/heart-full.fbbe3f46.webp"
+  }
+]);
